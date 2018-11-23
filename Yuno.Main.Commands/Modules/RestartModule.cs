f10644d5a4839f58101d142a0062614e1a42bfdf
Damian Kraaijeveld <Damian.Kraaijeveld96@gmail.com>
2018-11-23 01:15:54 +0100

    Added restart module

---
+++ Yuno.Main.Commands/Modules/RestartModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Yuno.Main.Restart;
+
+namespace Yuno.Main.Commands.Modules
+{
+    [RequireOwner]
+    public class RestartModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("restart")]
+        public async Task RestartCommand()
+        {
+            await Context.Message.DeleteAsync();
+            await ReplyAsync("Restarting...");
+            RestartHandler.Restart();
+        }
+
+        [Command("shutdown")]
+        public async Task ShutdownCommand()
+        {
+            await Context.Message.DeleteAsync();
+            await ReplyAsync("Shutting down...");
+            RestartHandler.Shutdown();
+        }
+    }
+}
