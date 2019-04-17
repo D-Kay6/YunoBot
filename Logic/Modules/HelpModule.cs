d58eade7ddc69e15f01ebeb4b92b154e6c7788e8
Damian Kraaijeveld <Damian.Kraaijeveld96@gmail.com>
2019-04-17 12:40:32 +0200

    Added invite command.

--- Logic/Modules/HelpModule.cs
+++ Logic/Modules/HelpModule.cs
@@ -2,6 +2,7 @@
 using Logic.Data;
 using Logic.Extentions;
 using System.Threading.Tasks;
+using Discord;
 
 namespace Logic.Modules
 {
@@ -19,22 +20,22 @@ namespace Logic.Modules
                 return;
             }
 
-            var ember = EmbedExtentions.CreateEmbed("Help", $@"Here is a list of commands you can use.
+            var embed = new EmbedBuilder();
+            embed.AddField("Help", @"Here is a list of commands you can use.
+I am constantly updated with new features so keep an eye on this page.");
 
-__**General commands**__
-{prefix}help (command) - Show advanced help for a command.
+            embed.AddField("__General Commands__", $@"{prefix}help (command) - Show advanced help for a command.
+{prefix}invite - Ask me for my invite link.
 {prefix}pick (option1|option2|etc) - Select a random item from the options that are separated with |.
 {prefix}birthday (username) - Sing a song for a happy fellow.
-{prefix}kill (username) - Make me kill that user.
+{prefix}kill (username) - Make me kill that user.");
 
-__praise__
-{prefix}praise (username) - Send a positive message to that user.
+            embed.AddField("Praise", $@"{prefix}praise (username) - Send a positive message to that user.
 {prefix}praise someone - Send a positive message to a random person.
 {prefix}praise everyone - Send a positive message to everyone.
-{prefix}praise role (rolename) - Send a positive message to a group.
+{prefix}praise role (rolename) - Send a positive message to a group.");
 
-__music__
-{prefix}music play (video url) - Add a song to the queue.
+            embed.AddField("music", $@"{prefix}music play (video url) - Add a song to the queue.
 {prefix}music play (name) - Search for a song by it's name and add it to the queue.
 {prefix}music pause - Pause the currently playing song.
 {prefix}music unpause - Un-pause the currently playing song.
@@ -43,34 +44,30 @@ __music__
 {prefix}music shuffle - Shuffle all the songs in the queue.
 {prefix}music skip - Skip the song currently playing.
 {prefix}music clear - Remove all songs from the queue.
-{prefix}music stop - Stop playing music and remove all songs from the queue.
+{prefix}music stop - Stop playing music and remove all songs from the queue.");
 
-__**Admin commands**__
-{prefix}announce (message) - Announce a message to all members of your server.
+            embed.AddField("request", $@"{prefix}request feature - Send a message to my developer with the request for a new feature.
+{prefix}request change - Send a message to my developer with the request for a change of an already existing feature.");
 
-__prefix__
-{prefix}prefix - Show the command prefix for this server.
-{prefix}prefix set (new prefix) - Change the command prefix for this server.
+            embed.AddField("__Admin Commands__", $@"{prefix}announce (message) - Announce a message to all members of your server.");
 
-__autochannel (ac)__
-{prefix}autochannel - Show the currently used icon for auto channels.
+            embed.AddField("prefix", $@"{prefix}prefix - Show the command prefix for this server.
+{prefix}prefix set (new prefix) - Change the command prefix for this server.");
+
+            embed.AddField("autochannel (ac)", $@"{prefix}autochannel - Show the currently used icon for auto channels.
 {prefix}autochannel seticon (new icon) - Change the icon for auto channels.
-{prefix}autochannel fix - Fixes the problem where I am unable to remove generates auto channels.
+{prefix}autochannel fix - Fixes the problem where I am unable to remove generates auto channels.");
 
-__permachannel (pc)__
-{prefix}permachannel - Show the currently used icon for perma channels
-{prefix}permachannel seticon (new icon) - Change the icon for perma channels.
+            embed.AddField("permachannel (pc)", $@"{prefix}permachannel - Show the currently used icon for perma channels
+{prefix}permachannel seticon (new icon) - Change the icon for perma channels.");
 
-__autorole (ar)__
-{prefix}autorole - Show the currently used icon for auto roles.
-{prefix}autorole seticon (new icon) - Change the icon for auto roles.
+            embed.AddField("autorole (ar)", $@"{prefix}autorole - Show the currently used icon for auto roles.
+{prefix}autorole seticon (new icon) - Change the icon for auto roles.");
 
-__permarole (pr)__
-{prefix}permarole - Show the currently used icon for perma roles
-{prefix}permarole seticon (new icon) - Change the icon for perma roles.
+            embed.AddField("permarole (pr)", $@"{prefix}permarole - Show the currently used icon for perma roles
+{prefix}permarole seticon (new icon) - Change the icon for perma roles.");
 
-I am constantly updated with new features so keep an eye on this page.");
-            await ReplyAsync("", false, ember);
+            await ReplyAsync("", false, embed.Build());
         }
 
         [Command("praise")]
@@ -101,6 +98,17 @@ I am constantly updated with new features so keep an eye on this page.");
 {prefix}music skip - Skip the song currently playing.
 {prefix}music clear - Remove all songs from the queue.
 {prefix}music stop - Stop playing music and remove all songs from the queue.");
+            await ReplyAsync("", false, embed);
+        }
+
+        [Command("request")]
+        public async Task HelpRequest()
+        {
+            var prefix = CommandSettings.Load(Context.Guild.Id).Prefix;
+            var embed = EmbedExtentions.CreateEmbed("Help request", $@"Here is more information about 'request'.
+
+{prefix}request feature - Send a message to my developer with the request for a new feature.
+{prefix}request change - Send a message to my developer with the request for a change of an already existing feature.");
             await ReplyAsync("", false, embed);
         }
 
---
+++ Logic/Modules/InviteModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace Logic.Modules
+{
+    [Group("Invite")]
+    public class InviteModule : ModuleBase<SocketCommandContext>
+    {
+        [Command]
+        public async Task DefaultInvite()
+        {
+            var channel = await Context.User.GetOrCreateDMChannelAsync();
+            await channel.SendMessageAsync(@"You can invite me to your server with this link: https://discordapp.com/oauth2/authorize?client_id=286972781273546762&scope=bot&permissions=8.
+For more info on me, go take a look at https://discordbots.org/bot/286972781273546762, or use /help in any of the discord servers I'm in.");
+        }
+    }
+}
