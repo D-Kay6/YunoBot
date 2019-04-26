2e0ece25f0c18adaef563078ef620c8d235ffa36
Damian Kraaijeveld <Damian.Kraaijeveld96@gmail.com>
2019-04-26 13:49:01 +0200

    Start working on poll module.

---
+++ Logic/Modules/PollModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace Logic.Modules
+{
+    [Group("poll")]
+    public class PollModule : ModuleBase<SocketCommandContext>
+    {
+        [Command]
+        public async Task DefaultPoll([Remainder] string message)
+        {
+            return;
+            await Context.Message.DeleteAsync();
+            var embed = new EmbedBuilder();
+            embed.WithDescription("A poll has started!");
+            await ReplyAsync();
+        }
+    }
+}
