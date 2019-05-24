cdb5fdbfc28ccfda0134ac162a610805f1eb35ec
Damian Kraaijeveld <Damian.Kraaijeveld96@gmail.com>
2019-05-24 11:57:21 +0200

    Added settings for database connection

---
+++ Dal/Json/Database.cs
@@ -0,0 +1,21 @@
+﻿using Dal.MySql;
+
+namespace Dal.Json
+{
+    internal class Database : Json
+    {
+        private const string Directory = "Configuration";
+
+        private const string File = "Database.json";
+
+        public Connection Read()
+        {
+            return Read<Connection>(Directory, File);
+        }
+
+        public void Write(Connection data)
+        {
+            Write(data, Directory, File);
+        }
+    }
+}
