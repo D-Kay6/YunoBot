a69373ed61d620a87bc62c8511e11bce8cdf37cc
Damian Kraaijeveld <Damian.Kraaijeveld96@gmail.com>
2019-05-24 11:57:04 +0200

    Added try catch for log writer.

--- Dal/Text/Logs.cs
+++ Dal/Text/Logs.cs
@@ -12,10 +12,17 @@ namespace Dal.Text
 
         public void Log(string file, string data)
         {
-            var directory = Path.Combine(Directory, file);
-            var path = Path.Combine(directory, $"{DateTime.Today:yy-MM-dd}.txt");
-            if (!System.IO.Directory.Exists(directory)) System.IO.Directory.CreateDirectory(directory);
-            _stream.WriteLine(path, $"[{DateTime.Now.ToShortTimeString()}] {data}");
+            try
+            {
+                var directory = Path.Combine(Directory, file);
+                var path = Path.Combine(directory, $"{DateTime.Today:yy-MM-dd}.txt");
+                if (!System.IO.Directory.Exists(directory)) System.IO.Directory.CreateDirectory(directory);
+                _stream.WriteLine(path, $"[{DateTime.Now.ToShortTimeString()}] {data}");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Failed to write log {file}.\n{data}.\n{e}.");
+            }
         }
     }
 }
