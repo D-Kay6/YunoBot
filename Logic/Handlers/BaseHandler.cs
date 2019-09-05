cf81eb6aa0eac9a4fd884adc77ec7190d087f81c
Damian Kraaijeveld <Damian.Kraaijeveld96@gmail.com>
2019-09-05 07:25:19 +0200

    Start working on inheritance with handlers

---
+++ Logic/Handlers/BaseHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+
+namespace Logic.Handlers
+{
+    public abstract class BaseHandler
+    {
+        protected DiscordSocketClient Client;
+
+        protected BaseHandler(DiscordSocketClient client)
+        {
+            this.Client = client;
+        }
+
+        public abstract Task Initialize();
+    }
+}
